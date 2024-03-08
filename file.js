83c5d8e2d3095d93b0410c4bdec4a768463f1abc
SyedFazal110 <syedfazal808@gmail.com>
2024-03-08 10:00:59 +0500

    js file created

---
+++ file.js
@@ -0,0 +1,14 @@
+// let names = ["Fazal" , "Ali" , "Faizan" , "Hamza" , "Hanzala"  , false , ["Hasan" , "qasim"]];
+// console.log(names);
+// console.log(names.length)
+
+
+
+
+// let car = "toyota";
+let cars = ["nissanGTR" , "mercedes" , "honda"];
+cars.push ("BMW");
+// cars.pop("")
+cars.unshift("ferrari" , "Lamborghini" , "Supra");
+cars.shift("")
+console.log(cars) 
