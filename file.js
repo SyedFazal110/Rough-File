32f308e9299bc53ee06609d2e0fce02be79b43e2
SyedFazal110 <syedfazal808@gmail.com>
2024-03-11 10:56:10 +0500

    js file created

--- file.js
+++ file.js
@@ -6,9 +6,49 @@
 
 
 // let car = "toyota";
-let cars = ["nissanGTR" , "mercedes" , "honda"];
-cars.push ("BMW");
+// let cars = ["nissanGTR" , "mercedes" , "honda"];
+// cars.push ("BMW");
 // cars.pop("")
-cars.unshift("ferrari" , "Lamborghini" , "Supra");
-cars.shift("")
-console.log(cars) 
+// cars.unshift("ferrari" , "Lamborghini" , "Supra");
+// cars.shift("")
+// console.log(cars) 
+
+
+
+
+// let bikes = ["Honda" , "KTM" , "Ducati" , "Yamaha" , "Ninja" , "Duke" , "Choper"];
+// bikes[1].pop();
+// bikes.splice(5 , 0 , "Kawasaki" , "BMW");
+// bikes.splice(3 , 2);
+// console.log(bikes);
+
+
+
+
+// let newArray = bikes.split["kawasaki"];
+// console.log(newArray)
+
+
+
+
+
+// const num = "10";
+// console.log(typeof(num))
+
+
+const fruits = ["Apple" , "Banana" , "Mango" , "Orange" , "Pineapple" , "Strawberry"]
+let check = document.querySelector("#check");
+function getInputValue(){
+    console.log(check.value)
+    if(fruits.includes(check.value)){
+        console.log('fruit available haa..')
+        h2.innerHTML = `${check.value} is available`
+    }
+    else{
+        console.log('fruit available nahi ha');
+        h2.innerHTML = `${check.value} is not available`
+    }
+    
+    
+    input.value = ''
+}
